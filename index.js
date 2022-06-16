4615c1d65b3a1c1398e320040da061472d5baabd
Marek <marek@ilieth.at>
2022-06-16 09:49:55 +0200

    Add frontend js

---
+++ index.js
@@ -0,0 +1,53 @@
+const input = document.querySelector('input');
+const label = document.querySelector('label');
+const video = document.querySelector('video');
+
+const upload_video = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    
+    reader.addEventListener('loadend', (e) => {
+        const buffer = e.target.result;
+        const video_blob = new Blob([new Uint8Array(buffer)], { type: 'video/mp4' });
+        const temporary_video_frontend_url = window.URL.createObjectURL(video_blob);
+        const fd = new FormData();
+        const is_production = document.body.dataset.prod;
+
+        fd.append('video', file, file.name);
+        manage_button_state('Uploading...', 'pending');
+
+        fetch(is_production ? 'prodlink' : 'http://localhost:5000/video/', {
+            method: 'POST',
+            body: fd,
+        }).then((data) => {
+            setTimeout(() => {
+                if (data.ok) {
+                    manage_button_state('Uploaded!', 'done');
+
+                    video.src = temporary_video_frontend_url;
+    
+                    setTimeout(() => {
+                        manage_button_state('Upload', 'waiting');
+                    }, 1000)
+                } else {
+                    alert ("Something went wrong");
+                }
+            }, 1000)
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    })
+
+    reader.readAsArrayBuffer(file);
+}
+
+const manage_button_state = (text, layout) => {
+    label.textContent = text;
+    label.className = layout;
+}
+
+input.addEventListener('change', (e) => upload_video(e));
