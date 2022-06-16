a2909c6fe913e6f28fdcd15bd6ef30f7914fd984
Marek <marek@ilieth.at>
2022-06-16 11:53:34 +0200

    Add removing existing video function when new one is being uploaded

--- server.js
+++ server.js
@@ -11,31 +11,48 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// MULTER
+
 const valid_track_format = (trackMimeType) => {
     const mimetypes = ["video/mp4"];
     return mimetypes.indexOf(trackMimeType) > -1;
-  }
+}
   
-  const track_file_filter = (req, file, cb) => {
+const track_file_filter = (req, file, cb) => {
     cb(null, valid_track_format(file.mimetype));
-  }
+}
   
-  const upload = multer({
+const upload = multer({
     storage: multer.diskStorage({
-      destination: (req, file, callback) => {  
-        const server_path = `${path.dirname(require.main.filename)}`;
+        destination: (req, file, callback) => {
 
-        fs.mkdirsSync(server_path);
-        callback(null, server_path);
-      },
-      filename: (req, file, callback) => {
-        const filename = 'video.mp4';
-        
-        callback(null, filename);
-      }
+            const server_path = `${path.dirname(require.main.filename)}/video`;
+
+            const remove_old_video = () => {
+                fs.readdir(server_path, (err, files) => {
+                    if (err) throw err;
+      
+                    for (const file of files) {
+                        fs.unlink(path.join(server_path, file), err => {
+                            if (err) throw err;
+                        });
+                    }
+                });
+            }
+
+            if (fs.existsSync(server_path)) remove_old_video();
+            fs.mkdirsSync(server_path);
+            callback(null, server_path);
+
+        },
+        filename: (req, file, callback) => {      
+            callback(null, 'video.mp4');
+        }
     }),
     fileFilter: track_file_filter
-  });
+});
+
+// ROUTES
 
 app.post('/video', upload.single('video'), (req, res) => {
     return res.json({ok: 1})
